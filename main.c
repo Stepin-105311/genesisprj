73c01a1a84b6fe552ddd30b566546cfe7777af1e
Naveen Pappuru <69245805+NAVEENPAPPURU@users.noreply.github.com>
2020-11-05 11:24:27 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Weekday.h"
+void main ()
+{
+  
+  char dayNames[20];
+  int c;
+  c=Weekday();
+  printf("week day is: %s\n",dayNames[c]);
+  
+}
+
+
+
---
+++ weekday.h
@@ -0,0 +1,6 @@
+#ifndef WEEKDAY_H_INCLUDED
+#define WEEKDAY_H_INCLUDED
+
+void Weekday();
+
+#endif
